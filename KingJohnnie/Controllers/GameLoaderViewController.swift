e152cb171ed3b889d539344ccfae04587027bff5
VolberdWork <volodymyr.kolomyltsvev@businessgroup.ltd>
2023-02-08 00:46:39 +0200

    loadingAnimation

--- KingJohnnie/Controllers/GameLoaderViewController.swift
+++ KingJohnnie/Controllers/GameLoaderViewController.swift
@@ -1,14 +1,22 @@
 import UIKit
+import Lottie
 
 class GameLoaderViewController: UIViewController {
-
+    var loadingAnimation = LottieAnimationView(name: "92298-loading")
+    
+    @IBOutlet var animationView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
        
+        loadingAnimation.frame = animationView.frame
+        self.view.addSubview(loadingAnimation)
+        loadingAnimation.loopMode = .repeatBackwards(3.0)
+        loadingAnimation.play()
+        
     }
     
-
- 
-
+    
+    
+    
 }
