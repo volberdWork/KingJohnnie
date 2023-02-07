42275b62c1427e3ee768412cecece27b64c86954
VolberdWork <volodymyr.kolomyltsvev@businessgroup.ltd>
2023-02-07 23:25:57 +0200

    loader Screen

--- KingJohnnie/ViewController.swift
+++ KingJohnnie/ViewController.swift
@@ -1,17 +1,10 @@
-//
-//  ViewController.swift
-//  KingJohnnie
-//
-//  Created by newAccount on 07.02.2023.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Some")
+        
     }
 
 
