df74e997ffe71a227278c0c64903fdda07e25c3c
VolberdWork <volodymyr.kolomyltsvev@businessgroup.ltd>
2023-02-07 23:11:50 +0200

    Initial Commit

---
+++ KingJohnnie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KingJohnnie
+//
+//  Created by newAccount on 07.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
