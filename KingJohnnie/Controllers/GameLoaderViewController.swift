2f57abdaedd589b1d7384ed39af5a2bfa635c8bf
VolberdWork <volodymyr.kolomyltsvev@businessgroup.ltd>
2023-02-08 00:06:03 +0200

    game loading animated

---
+++ KingJohnnie/Controllers/GameLoaderViewController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class GameLoaderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+ 
+
+}
--- KingJohnnie/Controllers/SelectGameViewController.swift
+++ KingJohnnie/Controllers/SelectGameViewController.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-class ViewController: UIViewController {
+class SelectGameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
