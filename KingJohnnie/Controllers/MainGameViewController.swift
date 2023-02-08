de731d6881b1867fa31d8ad05055f5866d7d2f0b
VolberdWork <volodymyr.kolomyltsvev@businessgroup.ltd>
2023-02-08 16:12:14 +0200

    configure View for mainGameScreen

--- KingJohnnie/Controllers/MainGameViewController.swift
+++ KingJohnnie/Controllers/MainGameViewController.swift
@@ -2,15 +2,22 @@ import UIKit
 
 class MainGameViewController: UIViewController {
     
+    @IBOutlet var collectionView: UICollectionView!
     @IBOutlet var backgraundImage: UIImageView!
+    
+    var array: [Int] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
+        for i in 0...100{
+            self.array.append(i)
+        }
     }
     
     private func setupView(){
         backgraundImageConfige(image: Constants.Images.mainWinScreenImage)
+        self.collectionView.backgroundColor = .clear
     }
     
     private func backgraundImageConfige(image: String){
@@ -28,3 +35,26 @@ class MainGameViewController: UIViewController {
         openVC()
     }
 }
+
+
+extension MainGameViewController : UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "LevelCollectionViewCell", for: indexPath) as! LevelCollectionViewCell
+    
+        return cell
+    }
+    
+    
+}
+
+
+extension MainGameViewController: UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return  CGSize(width: 50, height: 50)
+    }
+}
+
---
+++ KingJohnnie/Views/CollectionCells/LevelCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  LevelCollectionViewCell.swift
+//  KingJohnnie
+//
+//  Created by newAccount on 08.02.2023.
+//
+
+import UIKit
+
+class LevelCollectionViewCell: UICollectionViewCell {
+    
+}
