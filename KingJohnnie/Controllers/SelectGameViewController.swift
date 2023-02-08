8474bb673ab36b5bdf09193402fc9484e36019e6
VolberdWork <volodymyr.kolomyltsvev@businessgroup.ltd>
2023-02-08 10:24:08 +0200

    setupView for SelectGameViewController

--- KingJohnnie/Controllers/SelectGameViewController.swift
+++ KingJohnnie/Controllers/SelectGameViewController.swift
@@ -1,20 +1,32 @@
 import UIKit
 
 class SelectGameViewController: UIViewController {
-
+    
+    @IBOutlet var backgroundImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
     }
     
-    private func setupView(){
+    private  func setupView(){
+        logoForNavigationBar()
+        backgraundImage(image: Constants.Images.mainGameScreenImage)
+    }
+    
+    private func backgraundImage(image: String){
+        self.backgroundImage.image = UIImage(named: image)
+    }
+    
+    private func logoForNavigationBar(){
         let logo = UIImage(named: Constants.Images.logo)
         let imageView = UIImageView(image:logo)
         self.navigationItem.titleView = imageView
         imageView.contentMode = .scaleAspectFit
-       
+    }
+    
+    
+    @IBAction func settingButtonPressed(_ sender: UIBarButtonItem) {
         
     }
-
 }
 
