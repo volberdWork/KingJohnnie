4a9dba5a843ce867d3030bdfa3ee4d9e4c3242d8
VolberdWork <volodymyr.kolomyltsvev@businessgroup.ltd>
2023-02-09 10:00:41 +0200

    morning push

--- KingJohnnie/Views/CollectionCells/LevelCollectionViewCell.swift
+++ KingJohnnie/Views/CollectionCells/LevelCollectionViewCell.swift
@@ -1,12 +1,16 @@
-//
-//  LevelCollectionViewCell.swift
-//  KingJohnnie
-//
-//  Created by newAccount on 08.02.2023.
-//
-
 import UIKit
 
 class LevelCollectionViewCell: UICollectionViewCell {
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
 }
