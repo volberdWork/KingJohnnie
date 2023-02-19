1f060bf29605072b17bbbd5be1e557e64778897b
VolberdWork <volodymyr.kolomyltsvev@businessgroup.ltd>
2023-02-20 01:06:15 +0200

    change backgrounds with shines

--- KingJohnnie/Controllers/SelectGameViewController.swift
+++ KingJohnnie/Controllers/SelectGameViewController.swift
@@ -1,15 +1,12 @@
 import UIKit
 
 class SelectGameViewController: UIViewController {
+    
     @IBOutlet var detailsButton: UIButton!
     @IBOutlet var pageControl: UIPageControl!
     @IBOutlet var backgroundImage: UIImageView!
     @IBOutlet var collectionView: UICollectionView!
-    
-    
     var numberOfItemCollectionView = 0
-    
-    
     var collectionArray: [SelectGameCellModel] = [
         SelectGameCellModel(
             gameName: "KING OF LIGHT",
@@ -39,78 +36,52 @@ class SelectGameViewController: UIViewController {
     ]
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
-        
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
-        
         detailsButton.titleLabel?.text = "DETAILS"
         detailsButton.titleLabel?.font = UIFont(name: Constants.FontsStrings.InterBold, size: 17)
         pageControl.numberOfPages = self.collectionArray.count
-        
-        
-        
     }
-    
     private  func setupView(){
         logoForNavigationBar()
         backgraundImageConfige(image: Constants.Images.mainGameScreenImage)
         congigurePageControl()
         confiureButton()
-        
-        
     }
-    
     private func confiureButton(){
-        
         self.collectionView.backgroundColor = .clear
         self.detailsButton.layer.cornerRadius = 10
         self.detailsButton.layer.borderWidth = 0.5
         let whiteColor = CGColor(red: 255, green: 255, blue: 255, alpha: 1)
         self.detailsButton.layer.borderColor = whiteColor
-        
     }
-    
-    
-    
-    
-    
     private func congigurePageControl(){
         self.pageControl.transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
-        
     }
-    
     private func backgraundImageConfige(image: String){
         self.backgroundImage.image = UIImage(named: image)
     }
-    
     private func logoForNavigationBar(){
         let logo = UIImage(named: Constants.Images.logo)
         let imageView = UIImageView(image:logo)
         self.navigationItem.titleView = imageView
         imageView.contentMode = .scaleAspectFit
     }
-    
     func openMaiGamenController(){
         let main = UIStoryboard(name: "Main", bundle: nil)
         if let vc = main.instantiateViewController(withIdentifier: "MainGameViewController") as? MainGameViewController  {
             navigationController?.pushViewController(vc, animated: true)
         }
     }
-    
     func openSettingController(){
         let main = UIStoryboard(name: "Main", bundle: nil)
         if let vc = main.instantiateViewController(withIdentifier: "SettingsViewController") as? SettingsViewController  {
             navigationController?.pushViewController(vc, animated: true)
         }
     }
-    
-    
-    
-    
     @IBAction func detailsButtonPressed(_ sender: UIButton) {
         if pageControl.currentPage == 0{
             openMaiGamenController()
@@ -122,24 +93,17 @@ class SelectGameViewController: UIViewController {
             let alert = UIAlertController(title: "Game Closed",
                                           message: "This game is closed until Johnnie gives you permission. The permission will be granted after passing level 50 in the royal game.",
                                           preferredStyle: .alert)
-            
             let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
             alert.addAction(okAction)
-            
             present(alert, animated: true, completion: nil)
-            
         }
-        
-        
     }
     
     @IBAction func settingButtonPressed(_ sender: UIBarButtonItem) {
         SettingsViewController().playSound()
         SettingsViewController().makeVibration()
     }
-    
 }
-
 
 extension SelectGameViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource, UICollectionViewDelegate {
     
