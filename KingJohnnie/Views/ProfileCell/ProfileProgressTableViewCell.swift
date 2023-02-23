edd036b8f7181da98eeb41c6888943c8954c9771
VolberdWork <volodymyr.kolomyltsvev@businessgroup.ltd>
2023-02-23 10:07:31 +0200

    fix view for profileCells

--- KingJohnnie/Views/ProfileCell/ProfileProgressTableViewCell.swift
+++ KingJohnnie/Views/ProfileCell/ProfileProgressTableViewCell.swift
@@ -25,6 +25,8 @@ class ProfileProgressTableViewCell: UITableViewCell {
         winLabel.text = "Win     \(model.winCounts)"
         lossLabel.text = "Loss  \(model.lossCount)"
         
+       
+        
         let procentNumber = (Double(model.winCounts)/Double((Float(model.winCounts)+Float(model.lossCount))))*100.0
         let formattedNumber = String(format: "%.1f", procentNumber)
         
@@ -32,21 +34,30 @@ class ProfileProgressTableViewCell: UITableViewCell {
         levelGameLabel.text = "Level \(model.currentLevel)"
         levelGameLabel.textColor = .white
         levelGameLabel.font = UIFont(name: Constants.FontsStrings.InterMedium, size: 15)
+        if model.winCounts == 0 && model.lossCount == 0{
+            winLabel.textColor = .systemOrange
+            lossLabel.textColor = .systemOrange
+            winProcentLabel.text = "Win%  0"
+            winProcentLabel.textColor = .systemOrange
+        }
         
         winProgressView.lineWidth = 10
         winProgressView.startColor = Constants.Colors.orangeColor
         winProgressView.endColor = Constants.Colors.orangeColor
         winProgressView.setProgress(Float(model.winCounts)/Float((model.winCounts + model.lossCount)), animated: true)
+        winProgressView.grooveColor = Constants.Colors.alphaOrange
         
         lossProgressView.lineWidth = 10
         lossProgressView.startColor = Constants.Colors.orangeColor
         lossProgressView.endColor = Constants.Colors.orangeColor
         lossProgressView.setProgress(Float(model.lossCount)/Float((model.winCounts + model.lossCount)), animated: true)
+        lossProgressView.grooveColor = Constants.Colors.alphaOrange
         
         procentProgressView.lineWidth = 10
         procentProgressView.startColor = Constants.Colors.orangeColor
         procentProgressView.endColor = Constants.Colors.orangeColor
         procentProgressView.setProgress(Float(model.winCounts)/Float((model.winCounts + model.lossCount)), animated: true)
+        procentProgressView.grooveColor = Constants.Colors.alphaOrange
         
         gameNameLabel.text = model.nameOfGame
         gameNameLabel.textColor = .white
